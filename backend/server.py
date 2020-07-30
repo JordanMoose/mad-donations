7623645e8abe2592465c2804021781e678d9700b
Daniel Lurie <dlurie24@gmail.com>
2020-07-30 16:23:33 -0700

    got database connected

--- backend/server.py
+++ backend/server.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 MONGO_URI = "mongodb+srv://admin:admin@cluster0.vmafb.mongodb.net/Cluster0?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
+db = client['mad-donations']
+
 
 @app.route("/")
 def hello():
-    db = client['mad-donations']
     user = db.users.find_one()
     return user['name']
     
@@ -23,6 +24,14 @@ def users():
 @app.route("/users/<string:name>/")
 def getMember(name):
     return "Hello %s" % name
+
+@app.route("/users/add/<string:name>/")
+def addMember(name):
+    post = {
+        'name': name,
+    }
+    user_id = db.users.insert_one(post).inserted_id
+    return "Here is user_id: %s" % user_id
 
 if __name__ == "__main__":
     app.run()
