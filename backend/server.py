d2a0753834ac942e8652149a865df273d57c4282
Daniel Lurie <dlurie24@gmail.com>
2020-08-03 16:12:23 -0700

    added routes

--- backend/server.py
+++ backend/server.py
@@ -32,6 +32,30 @@ def addMember(name):
     user_id = db.users.insert_one(post).inserted_id
     return "Here is user_id: %s" % user_id
 
+@app.route("/home/")
+def home():
+    return "This is the homepage"
+
+@app.route("/login/")
+def login():
+    return "This is the login page"
+
+@app.route("/account/")
+def account():
+    return "You shouldn't be here!"
+
+@app.route("/account/<int:id>/")
+def getInfo(id):
+    return "This is account %s" % id
+
+@app.route("/orgs/")
+def orgs():
+    return "Here are all of our orgs"
+
+@app.route("/orgs/<int:id>")
+def getOrg(id):
+    return "This is organization %s" % id
+    
 if __name__ == "__main__":
     app.run()
     client.close()
