ce2a0fff5fbe70f10d3caf33f17649f1bfe7817d
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2020-08-03 18:23:17 -0700

    start work on homepage carousel and project structure
    fix gitignore
    
    move public directory back to root and initialize jsconfig.json
    
    create assets folder and fix imports

--- website/src/index.js
+++ website/src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import Routes from "./pages/Routes";
 import * as serviceWorker from "./serviceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
--- website/src/pages/home/Home.js
+++ website/src/pages/home/Home.js
@@ -1,6 +1,59 @@
-import React from "react";
-import "./Home.css";
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './Home.css';
+import hag from 'assets/images/hag.png';
+import polac from 'assets/images/polac.jpg';
+import unnamed from 'assets/images/unnamed.png';
 
 export default () => {
-  return <div>Hello world!</div>;
-};
+  const [carouselIndex, setCarouselIndex] = useState(0);
+  const handleCarouselSelect = (selectedIndex, e) => {
+    setCarouselIndex(selectedIndex);
+  };
+
+  return (
+    // <MenuBar activeTab="home" />
+    // Homepage image carousel
+    <div className="slideshow">
+      <Carousel activeIndex={carouselIndex} onSelect={handleCarouselSelect}>
+        <Carousel.Item className="each-slide">
+          <img
+            className="each-img"
+            src={hag}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="each-slide">
+          <img
+            className="each-img"
+            src={polac}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="each-slide">
+          <img
+            className="each-img"
+            src={unnamed}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
