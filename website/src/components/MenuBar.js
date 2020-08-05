3f634984350aad17752e00f1e6d7143555b2448c
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2020-08-05 15:08:53 -0700

    fix styling and link log in button

--- website/src/components/MenuBar.js
+++ website/src/components/MenuBar.js
@@ -8,14 +8,14 @@ import './MenuBar.scss';
 
 export default () => {
 	return (
-		<Navbar className="menubar" bg="dark">
+		<Navbar className="menubar" bg="dark" variant="dark" sticky>
 			<Navbar.Brand href="/home">MAD Donations</Navbar.Brand>
 			<Nav>
 				<Nav.Link href="/about">About</Nav.Link>
 				<Nav.Link href="/orgs">Organizations</Nav.Link>
 				<Nav.Link href="/contact">Contact Us</Nav.Link>
 			</Nav>
-			<Button>Log In</Button>
+			<Button href="/login">Log In</Button>
 		</Navbar>
 	)
 }
