e2fb7897c7dfa97cb0ecda25b30b8ab2c89d34e3
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2020-08-05 15:24:38 -0700

    add bootstrap styling to index.js

--- website/src/index.js
+++ website/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./pages/Routes";
 import * as serviceWorker from "./serviceWorker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
