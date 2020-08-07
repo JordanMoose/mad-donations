1c1d643cc9d4226e41ceb29fa83287343d24f104
Danielle Lurie <dlurie24@gmail.com>
2020-08-06 17:38:40 -0700

    added signout button, and conditional welcome message

--- website/src/components/menubar/MenuBar.js
+++ website/src/components/menubar/MenuBar.js
@@ -5,8 +5,19 @@ import {
 	Button
 } from 'react-bootstrap';
 import './MenuBar.scss';
+import { Redirect } from 'react-router-dom';
+import * as firebase from "firebase/app";
+import 'firebase/auth'
 
 export default (activeTab) => {
+	const handleClick = () => {
+		firebase.auth().signOut().then(() => {
+			return <Redirect to="/home" />
+		}).catch(() => {
+			console.log("FAILED TO SIGN OUT")
+		})
+	}
+
 	return (
 		<Navbar sticky bg="dark" variant="dark">
 			<Navbar.Brand href="/home">MAD Donations</Navbar.Brand>
@@ -16,6 +27,8 @@ export default (activeTab) => {
 				<Nav.Link href="/contact">Contact Us</Nav.Link>
 			</Nav>
 			<Button href="/login">Log In</Button>
+			<p id='user-display-name' style={{color: 'white'}}></p>
+			<Button onClick={handleClick}>Sign Out</Button>
 		</Navbar>
 	)
 }
