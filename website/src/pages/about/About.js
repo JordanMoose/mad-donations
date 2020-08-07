9e5d43e12132377de484fa62b8161178058ed54a
Danielle Lurie <dlurie24@gmail.com>
2020-08-06 17:39:03 -0700

    added menubar to these pages

--- website/src/pages/about/About.js
+++ website/src/pages/about/About.js
@@ -1,23 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./About.css";
+import MenuBar from 'components/menubar/MenuBar.js';
 
 export default () => {
-  console.log('Render')
-  const [currentUser, setCurrentUser] = useState("baljeet");
-
-  useEffect(() => {
-    fetch("/getUser/name/Danielle")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setCurrentUser(data.firstName);
-      });
-  }, []);
 
   return (
-    <div>
-      <div>This is the about page.</div>
-      <div>Current user: {currentUser}</div>
-    </div>
+    <>
+      <MenuBar activeTab='/about' />
+      <div>
+        <div>This is the about page.</div>
+      </div>
+    </>
   );
 };
--- website/src/pages/login/Login.js
+++ website/src/pages/login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Login.css";
+import MenuBar from 'components/menubar/MenuBar.js';
 
 export default () => {
   console.log("Hello");
 
   return (
-    <html>
+    <>
         <head>
             <script src="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.js"></script>
             <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.6.1/firebase-ui-auth.css" />
@@ -15,6 +16,6 @@ export default () => {
             <div id="firebaseui-auth-container"></div>
             <div id="loader">Loading...</div>
         </body>
-    </html>
+    </>
   );
 };
