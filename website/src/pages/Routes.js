1be0ba66699746ac498737497cb9cc28b526f0e2
Danielle Lurie <dlurie24@gmail.com>
2020-08-10 18:06:36 -0700

    added usercontext which tracks user across all pages. and signing out redirects to home

--- website/src/pages/Routes.js
+++ website/src/pages/Routes.js
@@ -7,43 +7,47 @@ import Login from "./login/Login"
 import Orgs from "./orgs/Orgs"
 import Subscribe from "./subscribe/Subscribe";
 import Home from './home/Home'
+import UserProvider from 'providers/UserProvider';
+
 
 export default () => {
   return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/account">
-            <Account />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/orgs">
-            <Orgs />
-          </Route>
-          <Route path="/subscribe">
-            <Subscribe />
-          </Route>
-          <Route path="/">
-            <Redirect to="/home" />
-          </Route>
-          <Route >
-            <div>You're not supposed to be here!</div>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <UserProvider>
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/account">
+              <Account />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/orgs">
+              <Orgs />
+            </Route>
+            <Route path="/subscribe">
+              <Subscribe />
+            </Route>
+            <Route path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route >
+              <div>You're not supposed to be here!</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserProvider>
   );
 };
