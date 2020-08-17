d13bd1e24c97089aa367f425f3c1c16454f6412b
Danielle Lurie <dlurie24@gmail.com>
2020-08-17 16:34:57 -0700

    menubar updates if user signed in, also added the initial choose genre page

--- website/src/components/menubar/MenuBar.js
+++ website/src/components/menubar/MenuBar.js
@@ -6,32 +6,47 @@ import {
 } from 'react-bootstrap';
 import './MenuBar.scss';
 import { Redirect } from 'react-router-dom';
-import * as firebase from "firebase/app";
-import 'firebase/auth'
+import { auth } from 'firebase.js'
 import { UserContext } from 'providers/UserProvider';
 
 export default (activeTab) => {
 	const user = useContext(UserContext);
 
 	const handleClick = () => {
-		firebase.auth().signOut().then(() => {
+		auth.signOut().then(() => {
 			return <Redirect to="/home" />
 		}).catch(() => {
 			console.log("FAILED TO SIGN OUT")
 		})
 	}
 
-	return (
-		<Navbar sticky bg="dark" variant="dark">
-			<Navbar.Brand href="/home">MAD Donations</Navbar.Brand>
-			<Nav activeKey={"/" + activeTab}>
-				<Nav.Link href="/about">About</Nav.Link>
-				<Nav.Link href="/orgs">Organizations</Nav.Link>
-				<Nav.Link href="/contact">Contact Us</Nav.Link>
-				{user ? <Nav.Link href="/account">Account</Nav.Link> : null}
-			</Nav>
-			{user ? <Button onClick={handleClick} href="/home">Sign Out</Button> : <Button href="/login">Log In</Button>}
-			<p id='user-display-name' style={{color: 'white'}}>{(user && ("What's up " + user.displayName)) || "Hello"}</p>
-		</Navbar>
-	)
+	var ret;
+	if (user != null) {
+		ret = (
+			<Navbar sticky bg="dark" variant="dark">
+				<Navbar.Brand href="/home">MAD Donations</Navbar.Brand>
+				<Nav activeKey={"/" + activeTab}>
+					<Nav.Link href="/about">About</Nav.Link>
+					<Nav.Link href="/orgs">Organizations</Nav.Link>
+					<Nav.Link href="/contact">Contact Us</Nav.Link>
+					<Nav.Link href="/account">Account</Nav.Link>
+				</Nav>
+				<p id='user-display-name' style={{color: 'white'}} href="/account">{"Signed in as " + user.displayName}</p>
+				<Button onClick={handleClick} href="/home">Sign Out</Button>
+			</Navbar>
+		)
+	} else {
+		ret = (
+			<Navbar sticky bg="dark" variant="dark">
+				<Navbar.Brand href="/home">MAD Donations</Navbar.Brand>
+				<Nav activeKey={"/" + activeTab}>
+					<Nav.Link href="/about">About</Nav.Link>
+					<Nav.Link href="/orgs">Organizations</Nav.Link>
+					<Nav.Link href="/contact">Contact Us</Nav.Link>
+				</Nav>
+				<Button href="/login">Log In</Button>
+			</Navbar>
+		)
+	}
+	return ret
 }
