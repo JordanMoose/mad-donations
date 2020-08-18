d2db6af0e6a50b14c3d260157a26c4ab292fdee7
Danielle Lurie <dlurie24@gmail.com>
2020-08-18 16:00:26 -0700

    basic frontend functionality for choosing causes

--- website/src/components/login/CauseSelect.js
+++ website/src/components/login/CauseSelect.js
@@ -1,23 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import './CauseSelect.css';
 
 export default () => {
+    const [submitted, setSubmitted] = useState(false)
+    const [userCauses, setUserCauses] = useState([])
     const causes = ["LGBTQ+ Rights", "Black Lives Matter", "Environmental Rights", "Healthcare"]
     
-    const handleSubmit = () => {
+    const handleChange = (event) => {
+        const target = event.target
+        if (target.checked) {
+            setUserCauses(userCauses.concat(target.value))
+        } else {
+            const newUserCauses = userCauses.filter(causeName => {
+                return causeName !== target.value
+            })
+            setUserCauses(newUserCauses)
+        }
+    }
+
+    const handleSubmit = (event) => {
+        // hit the route to send data to backend
+        
+        // on success
+        setSubmitted(true)
+        console.log(userCauses)
+        event.preventDefault()
+    }
+
+    if (submitted) {
+        return <Redirect to="/subscribe" />
     }
 
     return (
         <div className="cause-sel-wrapper">
             <form onSubmit={handleSubmit}>
-                {causes.map((name, i) => {
-                    return (
-                    <>
-                        <input type="checkbox" value={name} />
+                {(causes.map((name, i) => 
+                    <div key={i}>
+                        <input type="checkbox" value={name} onChange={handleChange}/>
                         <label>{name}</label>
                         <br />
-                    </>)
-                })}
+                    </div>
+                ))}
                 <input type="submit" value="Submit"/>
             </form>
         </div>
