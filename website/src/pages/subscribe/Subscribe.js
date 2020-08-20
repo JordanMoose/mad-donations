4fd118d49e397b0ef341b78a1273739faa3a65d9
Danielle Lurie <dlurie24@gmail.com>
2020-08-20 14:48:31 -0700

    successfully gets user's supported causes

--- website/src/pages/subscribe/Subscribe.js
+++ website/src/pages/subscribe/Subscribe.js
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Subscribe.css";
 import MenuBar from 'components/menubar/MenuBar.js'
+import { UserContext } from 'providers/UserProvider';
 
 export default () => {
+	const [userCauses, setUserCauses] = useState([])
+	const user = useContext(UserContext);
+
+	useEffect(() => {
+		if (user != null) {
+			const requestOptions = {
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' },
+			};
+			fetch("/user/" + user.email + "/causes/", requestOptions)
+			.then(res => res.text())
+			.then(text => {
+				setUserCauses(JSON.parse(text))
+
+			})
+			.catch(err => console.log(err))
+		}
+	})
+
+	if (user == null) {
+		return <div>Loading...</div>
+	}
 
   return (
     <>
       <MenuBar />
       <div>
-        <div>This is the subscribe page. Here, users will subscribe!</div>
+				{userCauses.map((cause, i) => 
+					<div key={i}>{cause}</div>
+				)}
       </div>
     </>
-  );
+  )
 };
