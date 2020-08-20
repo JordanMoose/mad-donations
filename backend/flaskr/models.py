83d9648d889e5f2e6f2c629037d2774b0674a4b7
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2020-08-20 15:57:17 -0700

    remove Category model and categories collection, remove ActiveMonth model, remove custom id field from each model, add Cause model and causes collection and change cause(s) field in each other model to reference it, remove transactionType field from Transaction model

--- backend/flaskr/models.py
+++ backend/flaskr/models.py
@@ -11,48 +11,35 @@ from mongoengine import (
     ListField
 )
 
+class Cause(Document):
+    name = StringField()
+    meta = {'collection': 'causes'}
+
+class Organization(Document):
+    name = StringField()
+    causes = ListField(ReferenceField(Cause))
+    monthsActive = ListField(StringField()) # e.g., January 2020
+    totalRaised = FloatField()
+    meta = {'collection': 'orgs'}
+
 class Subscription(Document):
-    subscriptionID = ObjectIdField()
-    cause = StringField()
+    cause = ReferenceField(Cause)
     monthlyAmount = FloatField()
     status = StringField()  # active, cancelled, or updated (if the user has since updated their monthly amount for this subscription)
     meta = {'collection': 'subscriptions'}
     
 class Transaction(Document):
-    transactionID = ObjectIdField()
     time = DateTimeField()
     amount = FloatField()
-    transactionType = StringField() # subscribe start, subscribe end, subscribe switch price/cause
-    cause = StringField()
+    cause = ReferenceField(Cause)
+    org = ReferenceField(Organization)
     meta = {'collection': 'transactions'}
 
 class User(Document):
-    userID = ObjectIdField()
     displayName = StringField()
     email = EmailField()
-    supportedCauses = ListField(StringField())
+    supportedCauses = ListField(ReferenceField(Cause))
     transactions = ListField(ReferenceField(Transaction))
     activeSubscriptions = ListField(ReferenceField(Subscription))
     expiredSubscriptions = ListField(ReferenceField(Subscription))
     meta = {'collection': 'users'}
-
-class Organization(Document):
-    orgID = ObjectIdField()
-    categories = ListField(StringField())
-    monthsActive = ListField(StringField()) # each item should be formatted like "8/20"
-    totalRaised = FloatField()
-    meta = {'collection': 'orgs'}
-
-class ActiveMonth(EmbeddedDocument):
-    # this class is for one month of one category
-    activeMonthID = ObjectIdField()
-    month = StringField() # should be formatted like "8/20"
-    orgs = ListField(ReferenceField(Organization))
-    totalRaised = FloatField()
-    notes = StringField()  # put other info here, like the cash split or whatever
-    
-class Category(Document):
-    categoryID = ObjectIdField()
-    name = StringField()
-    months = EmbeddedDocumentListField(ActiveMonth)
-    meta = {'collection': 'categories'}
