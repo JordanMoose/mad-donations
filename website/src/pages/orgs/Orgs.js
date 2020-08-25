b74d0719504468989b75fdee027f72a6082c74bc
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2020-08-25 15:07:00 -0700

    fetch org data from server and populate orgs page

--- website/src/pages/orgs/Orgs.js
+++ website/src/pages/orgs/Orgs.js
@@ -7,71 +7,61 @@ import "./Orgs.scss";
 import MenuBar from 'components/menubar/MenuBar.js';
 import holder from 'assets/images/placeholder.png';
 
-export default () => (
-  <>
-    <MenuBar activeTab="orgs" />
-    <div className="main">
-      <h1>Organizations</h1>
-      <h2 className="section-header">Next month's featured organizations</h2>
-      <ListGroup variant="flush">
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 1</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 2</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 3</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-      </ListGroup>
-      <h2 className="section-header">Organizations we've donated to in the past</h2>
-      <ListGroup variant="flush">
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 1</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 2</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="org">
-          <Image className="org-pic" src={holder} rounded />
-          <div className="org-info">
-            <h4 className="org-title">Organization 3</h4>
-            <p className="org-desc">
-              Info
-            </p>
-          </div>
-        </ListGroup.Item>
-      </ListGroup>
-    </div>
-  </>
-);
+export default () => {
+  const [featOrgs, setFeatOrgs] = useState([])
+  const [nonFeatOrgs, setNonFeatOrgs] = useState([])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    };
+    fetch("/orgs/featured/", requestOptions)
+      .then(res => res.json())
+      .then(featOrgsData => {
+        setFeatOrgs(featOrgsData)
+      })
+    fetch("/orgs/nonfeatured/", requestOptions)
+      .then(res => res.json())
+      .then(nonFeatOrgsData => {
+        setNonFeatOrgs(nonFeatOrgsData)
+      })
+  }, [])
+
+  return (
+    <>
+      <MenuBar activeTab="orgs" />
+      <div className="main">
+        <h1>Organizations</h1>
+        <h2 className="section-header">This month's featured organizations</h2>
+        <ListGroup variant="flush">
+          {(featOrgs.map((orgData, i) => 
+            <ListGroup.Item className="org" key={i}>
+              <Image className="org-pic" src={holder} rounded />
+              <div className="org-info">
+                <h4 className="org-title">{orgData.name}</h4>
+                <p className="org-desc">
+                  {orgData.mission}
+              </p>
+              </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        <h2 className="section-header">Organizations we've donated to in the past</h2>
+        <ListGroup variant="flush">
+          {(nonFeatOrgs.map((orgData, i) => 
+            <ListGroup.Item className="org" key={i}>
+              <Image className="org-pic" src={holder} rounded />
+              <div className="org-info">
+                <h4 className="org-title">{orgData.name}</h4>
+                <p className="org-desc">
+                  {orgData.mission}
+              </p>
+              </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    </>
+  );
+}
